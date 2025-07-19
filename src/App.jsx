05b78ed80768114a1497b3dc53b81fbd2095fc37
ndurushima <ndu1994@gmail.com>
2025-07-19 10:03:31 -0500

    added parent routes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Shop from "./pages/Shop"
+import Login from "./pages/Login"
 
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
